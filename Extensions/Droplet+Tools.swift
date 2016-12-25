65a55acbd9c3c7836dfe422ddf5c0771776cdefa
Ondrej Rafaj <pro@Super-Dictator.local>
2016-12-25 14:25:51 +0000

    initial upload

---
+++ Extensions/Droplet+Tools.swift
@@ -0,0 +1,18 @@
+//
+//  Droplet+Tools.swift
+//  Boost
+//
+//  Created by Ondrej Rafaj on 25/11/2016.
+//  Copyright © 2016 manGoweb UK Ltd. All rights reserved.
+//
+
+import Vapor
+
+
+extension Droplet {
+    
+    func register(controller: ControllerProtocol) {
+        controller.configureRoutes()
+    }
+    
+}
---
+++ Models/ModelHelpers.swift
@@ -0,0 +1,15 @@
+//
+//  ModelHelpers.swift
+//  Boost
+//
+//  Created by Ondrej Rafaj on 29/11/2016.
+//  Copyright © 2016 manGoweb UK Ltd. All rights reserved.
+//
+
+import Foundation
+
+typealias IdType = String
+
+
+
+
